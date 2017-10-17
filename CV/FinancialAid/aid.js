1f77ac06d496a0334f76f0f87b16c8ce5005c5df
jrhovet <jrhovet@gmail.com>
2017-10-16 22:16:38 -0400

    started financial aid story

---
+++ CV/FinancialAid/aid.js
@@ -0,0 +1,105 @@
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+
+width = width < 1000
+    ? width
+    : 1000
+
+var height = width < 1000
+    ? 600
+    : width * 0.6
+
+var padding = 50
+
+var grotonColor = "#8C1217"
+var secondaryColor = "#222222"
+
+var formatComma = d3.format(",");
+
+number = d3.select("#SVG_number")
+    // .attr("class", "left")
+
+numberGrid = new Grid(number, "#SVG_number")
+    .width(300)
+    .height(300)
+    .rows(20)
+    .cols(20)
+    .max(100)
+    .padding(1)
+    .mouseover(function(datum, index, selection, my) {
+        if(my.active){
+            d3.select("#numberAnswer")
+                .html((my.unit * (index + 1)))
+            }
+        // console.log("from callback ", my.max, my.total)
+        // console.log("amount ", my.unit * (index + 1))
+
+    })
+    .click(function(datum, index, selection, my) {
+        console.log("g1 click", datum, index, selection)
+
+    })
+
+numberGrid.call()
+
+grantBoarders = d3.select("#SVG_grantBoarders")
+
+grantGridBoarders = new Grid(grantBoarders, "#SVG_grantBoarders")
+    .width(300)
+    .height(300)
+    .rows(20)
+    .cols(20)
+    .max(56000)
+    .padding(1)
+    .mouseover(function(datum, index, selection, my) {
+        if(my.active){
+            d3.select("#grantBoardersAnswer")
+                .html(d3.format(",")(my.unit * (index + 1)))
+            }
+        // console.log("from callback ", my.max, my.total)
+        // console.log("amount ", my.unit * (index + 1))
+
+    })
+
+grantGridBoarders.call()
+
+grantDay = d3.select("#SVG_grantDay")
+
+grantGridDay = new Grid(grantDay, "#SVG_grantDay")
+    .width(300)
+    .height(300)
+    .rows(20)
+    .cols(20)
+    .max(56000)
+    .padding(1)
+    .mouseover(function(datum, index, selection, my) {
+        if(my.active){
+            d3.select("#grantBoardersDay")
+                .html(d3.format(",")(my.unit * (index + 1)))
+            }
+        // console.log("from callback ", my.max, my.total)
+        // console.log("amount ", my.unit * (index + 1))
+
+    })
+
+grantGridDay.call()
+
+
+
+
+
+
+
+
+//
+// g2 = new grid(d3.select("#two"), "#two")
+//     .mouseover(function(datum, index, selection, my) {
+//         console.log("g2")
+//         console.log("from callback ", my.max, my.total)
+//         console.log("amount ", my.unit * (index + 1))
+//
+//     })
+//     .click(function(datum, index, selection, my) {
+//         console.log("g2 click", datum, index, selection)
+//     })
+//
+// g2.call()
--- CV/d3Helpers.js
+++ CV/d3Helpers.js
@@ -1,3 +1,220 @@
+
+function Grid(selection, id) {
+    var id = id
+        width = 600, // default width
+        height = 600; // default height
+        rows = 10;
+        cols = 10;
+        className = "grid";
+        min = 0;
+        max = 1000000; //one million
+        padding = 2
+        fillColor = "#AEAEAE"
+        highlightColor = "#8C1217"
+        activeColor = "#222222"
+        active = true
+        mouseover = function(d, i, selection) {
+            return
+        }
+        mouseout = function(d, i, selection) {
+            return
+        }
+        click = function(d, i, selection) {
+            return
+        }
+
+
+
+    function my() {
+
+        var svg = selection
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", selection.attr("class") + " " + "gridSVG_" + className)
+
+        my.rows = rows
+        my.cols = cols
+        my.max = max
+        my.total = my.rows * my.cols
+        my.unit = my.max / my.total
+        squareDim = height/rows < width/cols ? height/rows : width/cols
+        values = []
+
+        my.mouseover = mouseover
+        my.mouseout = mouseout
+        my.click = click
+
+        for (var i = 0; i < my.total; i++) {
+            values.push({
+                "x" : i % rows * squareDim,
+                "y" : Math.floor(i/rows) * squareDim,
+                "i" : i
+            })
+        }
+
+        g = svg.append("g")
+            .attr("class", "gridG_" + className)
+            .attr("id", id)
+            .selectAll("rect")
+            .data(values)
+            .enter()
+            .append("rect")
+                .attr("x", (d) => {return d.x + padding})
+                .attr("y", (d) => {return d.y + padding})
+                .attr("width", (d) => {return squareDim - padding*2})
+                .attr("height", (d) => {return squareDim - padding*2})
+                .attr("fill", fillColor)
+                .on("mouseover", function(datum, index) {
+                    if(my.active){
+                        d3.select(id)
+                            .selectAll("rect")
+                            .attr("fill", (d, i) => {
+                                if(i <= index) {
+                                    return activeColor
+                                } else {
+                                    return fillColor
+                                }
+                            })
+                        }
+
+                    // console.log("mouseover", mouseover)
+                    // console.log("my.mouseover", my.mouseover())
+                    my.mouseover(datum, index, g, my)
+                }.bind(this))
+                .on("mouseout", (datum, index) => {
+
+                    my.mouseout(datum, index, g, my)
+                })
+                .on("click", function(datum, index) {
+                    // console.log("in primary callback: ", this.active)
+                    // console.log("in primary callback my: ", my.active)
+                    if(my.active){
+                        my.active = false
+                        d3.select(id)
+                            .selectAll("rect")
+                            .attr("fill", (d, i) => {
+                                if(i <= index) {
+                                    return highlightColor
+                                } else {
+                                    return fillColor
+                                }
+                            })
+                    } else {
+                        my.active = true
+                    }
+                    my.click(datum, index, g, my)
+                }.bind(this))
+
+    }
+
+    my.width = function(value) {
+        if (!arguments.length)
+            return width;
+        width = value;
+        return my;
+    };
+
+    my.height = function(value) {
+        if (!arguments.length)
+            return height;
+        height = value;
+        return my;
+    };
+
+    my.rows = function(value) {
+        if(!arguments.length)
+            return rows;
+        rows = value;
+        return my
+    }
+
+    my.cols = function(value) {
+        if(!arguments.length)
+            return cols
+        cols = value
+        return my
+    }
+
+    my.className = function(value) {
+        if(!arguments.length)
+            return className
+        className = value
+        return my
+    }
+
+    my.min = function(value) {
+        if(!arguments.length)
+            return min
+        min = value
+        return my
+    }
+
+    my.max = function(value) {
+        if(!arguments.length)
+            return max
+        max = value
+        return my
+    }
+
+    my.padding = function(value) {
+        if(!arguments.length)
+            return padding
+        padding = value
+        return my
+    }
+
+    my.fillColor = function(value) {
+        if(!arguments.length)
+            return fillColor
+        fillColor = value
+        return my
+    }
+
+    my.highlightColor = function(value) {
+        if(!arguments.length)
+            return highlightColor
+        highlightColor = value
+        return my
+    }
+
+    my.activeColor = function(value) {
+        if(!arguments.length)
+            return activeColor
+        activeColor = value
+        return my
+    }
+
+    my.mouseover = function(value) {
+        if(!arguments.length)
+            return mouseover
+        mouseover = value
+        return my
+    }
+
+    my.mouseout = function(value) {
+        if(!arguments.length)
+            return mouseout
+        mouseout = value
+        return my
+    }
+
+    my.click = function(value) {
+        if(!arguments.length)
+            return click
+        click = value
+        return my
+    }
+
+    my.active = function(value) {
+        if(!arguments.length)
+            return active
+        active = value
+        return my
+    }
+
+    return my;
+}
+
 function drawAxis(scale, yPos, tickValues=null, className="default"){
     var axis = d3.axisBottom()
         .scale(scale)
