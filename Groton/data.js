12dc22f67466667027d6b6b1d8271158ed3d9357
jrhovet <jrhovet@gmail.com>
2018-12-22 12:01:53 -0500

    initial commit; first prototype

---
+++ Groton/data.js
@@ -0,0 +1,110 @@
+var dataJson = [
+	{
+		"year": 1884,
+		"body": "Endicott Peabody founded Groton School."
+	},
+	{
+		"year": 1899,
+		"body": "Headmaster Peabody invited Booker T. Washington, a freed slave, to speak on campus. Mr. Washington spoke at Groton again in 1905."
+	},
+	{
+		"year": 1899,
+		"body": "The school was a success and the campus we know today was already taking shape: the Schoolhouse, still the hub of Groton’s Circle, was built in 1899, St. John’s Chapel in 1900, and the gym (now the Dining Hall) in 1903."
+	},
+	{
+		"year": 1904,
+		"body": "President Theodore Roosevelt, a close friend and relative by marriage of Endicott Peabody’s, spoke at Groton’s commencement."
+	},
+	{
+		"year": 1917,
+		"body": "Mr. Peabody shepherded the school through World War I: the Groton boys conducted military drills, and wheat was grown right on the Circle to help with the war effort. Many graduates went on to fight in WWI, and several gave their lives."
+	},
+	{
+		"year": 1920,
+		"body": "By 1920, Groton School had grown to about twenty-two faculty members and 180 students, including four children of one particularly prominent parent, President Theodore Roosevelt."
+	},
+	{
+		"year": 1929,
+		"body": "During the Depression, Groton School, thanks to a recent bequest, was able to offer aid to families who no longer could afford the tuition."
+	},
+	{
+		"year": 1931,
+		"body": "When FDR was governor of New York, he delivered Groton’s Prize Day speech. LINK"
+	},
+	{
+		"year": 1933,
+		"body": "Franklin Delano Roosevelt, a graduate of Groton’s Form of 1900, became president. Mr. Peabody, who had officiated at Franklin’s wedding to Eleanor, was invited to all four of FDR’s inaugurations and attended all but the last.1940, Endicott Peabody retired after leading Groton School for fifty-six years. The next headmaster, the Reverend John Crocker (Groton Form of 1918), remained at the helm for twenty-five years."
+	},
+	{
+		"year": 1941,
+		"body": "World War II led to austerity measures; with workers headed to war, the Groton boys picked up jobs around campus, such as cleaning, waiting tables, and shoveling coal."
+	},
+	{
+		"year": 1952,
+		"body": "Known for doing what simply was moral and right, Headmaster Crocker oversaw the admission of the first black student to Groton. LINKto A Visionary Headmaster, Winter 13 Q"
+	},
+	{
+		"year": 1963,
+		"body": "Mr. Crocker invited Dr. Martin Luther King, Jr. to speak at Groton; the civil rights leader spent two days on campus. LINKto MLK page"
+	},
+	{
+		"year": 1965,
+		"body": "Mr. Crocker took Groton boys to march with Dr. Martin Luther King, Jr. in Boston. Graduates today recall a headmaster who instilled a deep understanding of civil rights and social justice through his sermons and his actions. That year also marked Mr. Crocker’s retirement. Reverend Bertrand N. Honea, Jr. led the school from 1965 to 1969."
+	},
+	{
+		"year": 1969,
+		"body": "Paul Wright became headmaster and served until 1974; he was the first headmaster who was not a clergyman."
+	},
+	{
+		"year": 1970,
+		"body": "The board designated a coeducation committee, and its proposal was approved by the trustees a year later. The decision was so controversial that the proposal was given a second review, but ultimately reaffirmed."
+	},
+	{
+		"year": 1974,
+		"body": "The Reverend Rowland Cox became headmaster as the school was about to become coeducational. He served until his death in 1977. From 1977–78, Peter Camp was acting headmaster."
+	},
+	{
+		"year": 1975,
+		"body": "The school welcomed the first female students to campus. Among the many changes accompanying coeducation was the hiring of full-time female faculty. Today, approximately half of the faculty are women and half of Groton students are girls."
+	},
+	{
+		"year": 1978,
+		"body": "William Polk ’58 became headmaster and served until 2003."
+	},
+	{
+		"year": 2002,
+		"body": "The Dillon Art Center opened."
+	},
+	{
+		"year": 2003,
+		"body": "Richard Commons became headmaster and served until 2013."
+	},
+	{
+		"year": 2005,
+		"body": "The Campbell Performing Arts Center opened."
+	},
+	{
+		"year": 2008,
+		"body": "The school initiated a policy waiving tuition for families earning less than $75,000/year (now $80,000)."
+	},
+	{
+		"year": 2013,
+		"body": "The current headmaster, Temba Maqubela, joined Groton."
+	},
+	{
+		"year": 2014,
+		"body": "The Board of Trustees embraced the GRoton Affordability and INclusion (GRAIN) initiative, naming it the school’s number-one strategic priority. GRAIN ensured that the school would meet each applicant’s full financial need and froze tuition for three years."
+	},
+	{
+		"year": 2015,
+		"body": "The newly renovated and expanded Schoolhouse opened. The project integrated the stately original structure—which was virtually unchanged—with a significant expansion that includes the fifty-foot-high Sackett Forum and state-of-the-art science classrooms and laboratories. It was the sixth renovation to the 1899 Schoolhouse."
+	},
+	{
+		"year": 2017,
+		"body": "On Christmas Eve, GRoton Affordability and INclusion (GRAIN) hit its $50 million milestone. It went on to raise more than $53 million."
+	},
+	{
+		"year": 2018,
+		"body": "Architectural Digest named Groton School’s Circle the most beautiful independent school campus in Massachusetts. Designed by landscape architect Frederick Law Olmsted (Central Park, Boston Public Garden), the Circle today looks largely as it did one hundred years ago."
+	}
+]
